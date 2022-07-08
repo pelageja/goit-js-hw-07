a38589cf35eaa746b3631313f879541b3b703f3c
pelageja <29102014n@gmail.com>
2022-07-08 16:45:14 +0300

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,77 +7,66 @@ console.log(galleryItems);
 
 
 
+const ulRef = document.querySelector('.gallery');
 
-//! Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
 
-//Находим ссылку, в которую нужно добавить нашу разметку
-const listGallary = document.querySelector(".gallery");
+
+const cartMarkup = createPictureCartMarkup(galleryItems);
+ulRef.insertAdjacentHTML('beforeend', cartMarkup);
+
+
+
+
+
+
+
+
 //Создаем функцию для создания разметки
-const makeGallaryCards = ({ preview, original, description }) => {
-  return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
+
+function createPictureCartMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+   <a class="gallery__link" href="${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+     />
+   </a>
+ </div>`;
+    })
+    .join('');
 };
-//Перебираем массив с помощью метода map() и возвращаем массив созданных шаблонных строк
-const galleryArray = galleryItems.map((el) => {
-  return makeGallaryCards(el);
-});
-//добавляем в разметку шаблонные строки, перед этим их соединяем в одну строку методом join();
-//так как метод insertAdjacentHTML() - работает с строкой
-listGallary.insertAdjacentHTML("afterbegin", galleryArray.join(""));
 
-/*
-! Реализация делегирования на div.gallery и получение url большого изображения.
 
-! Подключение скрипта и стилей библиотеки модального окна basicLightbox. 
-! Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
-*/
-//Создаем функцию
-const onlistGallaryClick = (event) => {
-  event.preventDefault(); // делаем, чтобы страница не перезагружалась
-  //делаем проверку на то, сто кликаем именно в тег IMG
-  if (event.target.tagName !== "IMG") {
+
+
+//Создаем функцию,чтобы страница не перезагружалась и то что кликаем именно на тег IMG
+
+let instance;
+function onPictureClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
     return;
   }
-
-  //обьявляем пернменные, чтобы найти src и href
-  let imgSRC = event.target.src;
-  const linkHREF = event.target.parentNode.href;
-  //присваем значению src значение href
-  imgSRC = linkHREF;
-  //подключаем моальое окно из библиотеки  basicLightbox (до этого подключив скрипт js и css в index.html)
-  const instance = basicLightbox.create(
-    `
-    <img src="${linkHREF}" class="gallery__image"
->
-`, //вторым параметром добавляем обьект, в котором будет 2 функции
-    {
-      onShow: (instance) => {
-        //вешаем слушателя событий на window
-        window.addEventListener("keydown", onEscPressKey);
-      },
-      onClose: (instance) => {
-        //убираем слушателя событий на window
-        window.removeEventListener("keydown", onEscPressKey);
-      },
-    }
+ 
+  instance = basicLightbox.create(`<img src="${event.target.dataset.source}" />`
   );
-  //вызов функции show()
+
   instance.show();
-  //создаем функцию закрытия окна по Escape
-  function onEscPressKey(event) {
-    if (event.code === "Escape") {
-      instance.close(); //вызов функции close()
+
+ window.addEventListener("keydown", onEcpBtnPush);
+
+  
+ 
+  function onEcpBtnPush(event) {
+    if (event.code !== "Escape") {
+      return;
     }
+    instance.close();
+    window.removeEventListener("keydown", onEcpBtnPush);
   }
 };
-
-//вешаем слушателя событий на div с классом class="gallery"
-listGallary.addEventListener("click", onlistGallaryClick);
+ulRef.addEventListener('click', onPictureClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,47 +3,35 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const ulRef = document.querySelector('.gallery');
 
 
+const cartMarkup = createPictureCartMarkup(galleryItems);
+ ulRef.insertAdjacentHTML('beforeend', cartMarkup);
 
 
-/*
-! Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. 
-! Используй готовый код из первого задания.
-*/
-//Находим ссылку, в которую нужно добавить нашу разметку
-const listGallary = document.querySelector(".gallery");
-//Создаем функцию для создания разметки
-const makeGallaryCards = ({ preview, original, description }) => {
-  return `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
+function createPictureCartMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a  href="${original}">
+   <img class="gallery__image"
+     src="${preview}" 
+    alt="${description}" />
+   </a>
+   </li>
+   `;
+    })
+    .join('');
+ 
 };
-//Перебираем массив с помощью метода map() и возвращаем массив созданных шаблонных строк
-const galleryArray = galleryItems.map((el) => {
-  return makeGallaryCards(el);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
 });
-//добавляем в разметку шаблонные строки, перед этим их соединяем в одну строку методом join();
-//так как метод insertAdjacentHTML() - работает со строкой
-listGallary.insertAdjacentHTML("afterbegin", galleryArray.join(""));
 
-const onlistGallaryClick = (event) => {
-  event.preventDefault();
-  if (event.target.tagName !== "IMG") {
-    return;
-  }
 
-  //обьявляем пернменные, чтобы найти src и href
-  let imgSRC = event.target.src;
-  const linkHREF = event.target.parentNode.href;
-  //присваем значению src значение href
-  imgSRC = linkHREF;
 
-  var lightbox = new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionDelay: 250,
-  });
-};
 
-//вешаем слушателя событий на div с классом class="gallery"
-listGallary.addEventListener("click", onlistGallaryClick);
+
